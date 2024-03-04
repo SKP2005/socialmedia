ad9eec23e1e925c9a1ed68dfa6cd99e5be658829
Sanjeet <sanjeetkumar6178@gmail.com>
2024-03-04 19:27:26 +0530

    login design done

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,33 @@
+import "./login.scss"
+
+function Login() {
+  return (
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Hello World</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing 
+            elit. Dolorum ipsa aliquid qui doloremque aspernatur rerum rem.
+             Nam quibusdam deleniti eaque doloribus eius obcaecati ex, sequi libero, excepturi 
+             totam repudiandae dicta veritatis maiores reiciendis sunt odio autem 
+             officiis ut ipsam molestias!
+          </p>
+          <span>Don't you have an accounr?</span>
+          <button>Register</button>
+
+        </div>
+        <div className="right">
+            <h1>Login</h1>
+            <form>
+                <input type="text" placeholder="Username" />
+                <input type="password" placeholder="Password" />
+                <button>Login</button>
+            </form>
+        </div>
+        </div>  
+    </div>
+  )
+}
+
+export default Login
